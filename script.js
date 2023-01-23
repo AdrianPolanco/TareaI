40febcf8fcdbddd4a4b937a2df26a0ad614efe48
Adrian Saul Polanco Ferrer <116674818+AdrianPolanco@users.noreply.github.com>
2023-01-23 11:05:26 -0400

    Mitad

--- script.js
+++ script.js
@@ -20,10 +20,13 @@ const spinner = document.querySelector("#spinner");
 const hobbies = document.querySelector(".container");
 const bike = document.querySelector(".bike");
 const development = document.querySelector(".development");
+const gaming = document.querySelector(".gaming");
+
 //Eventos
 document.addEventListener("DOMContentLoaded", cargar);
 bike.addEventListener("click", crearHTML);
-development.addEventListener("mouseenter", crearHTML);
+development.addEventListener("click", crearHTML);
+gaming.addEventListener("click", crearHTML);
 //Funciones
 
 function cargar(e) {
@@ -43,9 +46,13 @@ function crearHTML(e) {
     e.target.classList.remove("border");*/
     switch (clase) {
         case "bike":
-            const card = document.createElement("div");
-            card.classList.add("container-info", "container-info-day");
-            card.innerHTML = `<header class="card-header card-header-day">
+            const cardBike = document.createElement("div");
+            cardBike.classList.add(
+                "container-info",
+                "container-info-day",
+                "my-bike"
+            );
+            cardBike.innerHTML = `<header class="card-header card-header-day">
             <p>Montar bicicleta</p>
         </header>
         <img
@@ -61,12 +68,67 @@ function crearHTML(e) {
                 en mi cara.
             </p>
         </div>`;
-            e.target.parentElement.insertBefore(card, e.target);
+            e.target.parentElement.insertBefore(cardBike, e.target);
             e.target.parentElement.removeChild(e.target);
-            card.addEventListener("click", limpiarDiv);
+            cardBike.addEventListener("click", limpiarDiv);
+
             break;
         case "development":
-            console.log("Desarrollo");
+            const cardDevelopment = document.createElement("div");
+            cardDevelopment.classList.add(
+                "container-info",
+                "container-info-day",
+                "my-development"
+            );
+            cardDevelopment.innerHTML = `
+            <header class="card-header card-header-day">
+                            <p>Programar</p>
+                        </header>
+                        <img
+                            src="images/development.jpg"
+                            alt="Software development"
+                            class="image development-img"
+                        />
+                        <div class="p-container">
+                            <p class="p p-day">
+                                Me gusta desarrollar aplicaciones web y aprender
+                                todo lo que tenga que ver con ello,
+                                especialmente el desarrollo web frontend
+                            </p>
+                        </div>`;
+            e.target.parentElement.insertBefore(cardDevelopment, e.target);
+            e.target.parentElement.removeChild(e.target);
+            cardDevelopment.addEventListener("click", limpiarDiv);
+
+            break;
+        case "gaming":
+            const cardGaming = document.createElement("div");
+            cardGaming.classList.add(
+                "container-info",
+                "container-info-day",
+                "my-gaming"
+            );
+            cardGaming.innerHTML = `
+            <header class="card-header card-header-day">
+                            <p>Videojuegos</p>
+                        </header>
+                        <img
+                            src="images/gaming.jpg"
+                            alt="Gaming"
+                            class="image gaming-img"
+                        />
+                        <div class="p-container">
+                            <p class="p p-day">
+                                Soy un gran fan de los videojuegos online,
+                                especialmente de consola o PC, son un gran hobby
+                                en mis tiempos libres para entretenerme y hacer
+                                amistades.
+                            </p>
+                        </div>`;
+            e.target.parentElement.insertBefore(cardGaming, e.target);
+            e.target.parentElement.removeChild(e.target);
+            cardGaming.addEventListener("click", limpiarDiv);
+
             break;
         default:
     }
@@ -74,24 +136,91 @@ function crearHTML(e) {
 
 function limpiarDiv(e) {
     if (e.target.classList.contains("container-info")) {
-        const newArticle = document.createElement("article");
-        newArticle.classList.add("hobbies", "bike", "border");
-        newArticle.innerHTML = ` <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="192"
-    height="160"
-    fill="currentColor"
-    class="bi bi-bicycle"
-    viewBox="0 0 16 16"
->
-    <path
-        d="M4 4.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1v.5h4.14l.386-1.158A.5.5 0 0 1 11 4h1a.5.5 0 0 1 0 1h-.64l-.311.935.807 1.29a3 3 0 1 1-.848.53l-.508-.812-2.076 3.322A.5.5 0 0 1 8 10.5H5.959a3 3 0 1 1-1.815-3.274L5 5.856V5h-.5a.5.5 0 0 1-.5-.5zm1.5 2.443-.508.814c.5.444.85 1.054.967 1.743h1.139L5.5 6.943zM8 9.057 9.598 6.5H6.402L8 9.057zM4.937 9.5a1.997 1.997 0 0 0-.487-.877l-.548.877h1.035zM3.603 8.092A2 2 0 1 0 4.937 10.5H3a.5.5 0 0 1-.424-.765l1.027-1.643zm7.947.53a2 2 0 1 0 .848-.53l1.026 1.643a.5.5 0 1 1-.848.53L11.55 8.623z"
-    />
-</svg>
-    `;
-        e.target.parentElement.insertBefore(newArticle, e.target);
-        e.target.parentElement.removeChild(e.target);
-        newArticle.addEventListener("click", crearHTML);
+        const clase = e.target.classList[2];
+        switch (clase) {
+            case "my-bike":
+                const newArticleBike = document.createElement("article");
+                newArticleBike.classList.add("hobbies", "bike", "border");
+                newArticleBike.innerHTML = ` <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="192"
+        height="160"
+        fill="currentColor"
+        class="bi bi-bicycle"
+        viewBox="0 0 16 16"
+    >
+        <path
+            d="M4 4.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1v.5h4.14l.386-1.158A.5.5 0 0 1 11 4h1a.5.5 0 0 1 0 1h-.64l-.311.935.807 1.29a3 3 0 1 1-.848.53l-.508-.812-2.076 3.322A.5.5 0 0 1 8 10.5H5.959a3 3 0 1 1-1.815-3.274L5 5.856V5h-.5a.5.5 0 0 1-.5-.5zm1.5 2.443-.508.814c.5.444.85 1.054.967 1.743h1.139L5.5 6.943zM8 9.057 9.598 6.5H6.402L8 9.057zM4.937 9.5a1.997 1.997 0 0 0-.487-.877l-.548.877h1.035zM3.603 8.092A2 2 0 1 0 4.937 10.5H3a.5.5 0 0 1-.424-.765l1.027-1.643zm7.947.53a2 2 0 1 0 .848-.53l1.026 1.643a.5.5 0 1 1-.848.53L11.55 8.623z"
+        />
+    </svg>
+        `;
+                e.target.parentElement.insertBefore(newArticleBike, e.target);
+                e.target.parentElement.removeChild(e.target);
+                newArticleBike.addEventListener("click", crearHTML);
+                break;
+            case "my-development":
+                const newArticleDevelopment = document.createElement("article");
+                newArticleDevelopment.classList.add(
+                    "hobbies",
+                    "development",
+                    "border"
+                );
+                newArticleDevelopment.innerHTML = ` <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="192"
+                height="192"
+                fill="#000000"
+                viewBox="0 0 256 256"
+            >
+                <rect width="256" height="256" fill="none"></rect>
+                <rect
+                    x="32"
+                    y="48"
+                    width="192"
+                    height="160"
+                    rx="8"
+                    fill="none"
+                    stroke="#000000"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="16"
+                ></rect>
+                <circle cx="68" cy="84" r="12"></circle>
+                <circle cx="108" cy="84" r="12"></circle>
+            </svg>
+        `;
+                e.target.parentElement.insertBefore(
+                    newArticleDevelopment,
+                    e.target
+                );
+                e.target.parentElement.removeChild(e.target);
+                newArticleDevelopment.addEventListener("click", crearHTML);
+                break;
+            case "my-gaming":
+                const newArticleGaming = document.createElement("article");
+                newArticleGaming.classList.add("hobbies", "gaming", "border");
+                newArticleGaming.innerHTML = ` <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="192"
+                height="160"
+                fill="currentColor"
+                class="bi bi-controller"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1v-1z"
+                />
+                <path
+                    d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729c.14.09.266.19.373.297.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466.206.875.34 1.78.364 2.606.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527-1.627 0-2.496.723-3.224 1.527-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.34 2.34 0 0 1 .433-.335.504.504 0 0 1-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a13.748 13.748 0 0 0-.748 2.295 12.351 12.351 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.354 12.354 0 0 0-.339-2.406 13.753 13.753 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27-1.036 0-2.063.091-2.913.27z"
+                />
+            </svg>
+        `;
+                e.target.parentElement.insertBefore(newArticleGaming, e.target);
+                e.target.parentElement.removeChild(e.target);
+                newArticleGaming.addEventListener("click", crearHTML);
+                break;
+            default:
+        }
     }
 }
 
